8a3b9e11baf4a2f1784e27b2e47c2cf6cb3f9592
Thiago Xavier <xavieroliva.thiagoe@gmail.com>
2024-04-24 23:29:25 -0300

    Agregada funcion delete() al Menu

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -14,9 +14,12 @@ func count_new_instance():
 	instancesCount += 1
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 
-
 func _on_red_generator_button_down():
 	count_new_instance()
 
 func _on_violet_generator_button_down():
 	count_new_instance()
+
+func delete():
+	instancesCount = 0
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
