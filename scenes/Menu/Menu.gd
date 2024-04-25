2476a98cd2adf5217f1baf3d75ff0c020949986a
Thiago Xavier <xavieroliva.thiagoe@gmail.com>
2024-04-24 23:27:46 -0300

    Conectado la escena Menu con los generadores

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -4,12 +4,19 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
 
+
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
-	pass
 
 func count_new_instance():
 	instancesCount += 1
-	$CountLabel.text = str(instancesCount)
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+
+
+func _on_red_generator_button_down():
+	count_new_instance()
+
+func _on_violet_generator_button_down():
+	count_new_instance()
